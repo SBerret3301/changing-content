101d5894eb15ff5a231606bea5c156dc6b4ad737
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-10-12 18:38:10 +0100

    Add files via upload

---
+++ changing_content.py
@@ -0,0 +1,7 @@
+x = input("Please write the first number : ")
+y = input("Write down the second number please :")
+z = x
+x = y 
+y = z
+print("now the first number is : "+x)
+print("and the second number is :"+y)
